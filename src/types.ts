ba33d204da8e4abecbf51090f4a559261e7b44cb
Basu Dev Adhikari <adbasudev54@gmail.com>
2023-08-03 18:52:43 +0545

    Refactor 1

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,7 @@ interface IMessage<TType extends string, TValue = undefined> {
     value?: TValue;
 }
 
-export type Message = IMessage<'hideShorts', boolean>
-    | IMessage<'log', any>
-    | IMessage<'reload'>
-    ;
+export type Message = IMessage<'reload'>;
 
 export type MessageType = Message['type'];
 export type MessageValueFromType<TType extends MessageType> = Extract<Message, { type: TType; }>['value'];
