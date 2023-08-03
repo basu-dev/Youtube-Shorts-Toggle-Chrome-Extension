6bc996f73de81aea5358c54ebaafa5c6fa1daa78
Basu Dev Adhikari <adbasudev54@gmail.com>
2023-08-03 18:42:08 +0545

    close out from shorts if you are watching shorts on toggle on

--- src/content/index.ts
+++ src/content/index.ts
@@ -5,17 +5,28 @@ const styleID = 'youtube-shorts-toggle-style';
 const youtubeShortsSectionSelector = '[is-shorts],ytd-reel-shelf-renderer';
 const youtubeShortsSidebarButtonSelector = "#endpoint[title='Shorts']";
 
-function toggleShorts(hide: boolean) {
-    if (!hide) {
-        let addedStyle = document.getElementById(styleID);
-        if (addedStyle) addedStyle.remove();
-        return;
-    }
+function removeStyles() {
+    let addedStyle = document.getElementById(styleID);
+    if (addedStyle) addedStyle.remove();
+    return;
+}
 
+function addStyles() {
     const styleElement = document.createElement('style');
     styleElement.id = styleID;
     styleElement.innerHTML = `${youtubeShortsSectionSelector},${youtubeShortsSidebarButtonSelector} { display: none !important; }`;
     document.head.appendChild(styleElement);
+}
+
+function checkForCurrenShortsActiveAndRemove() {
+    let isShortsActive = location.href.includes('/shorts/');
+    if (isShortsActive) history.back();
+}
+
+function toggleShorts(hide: boolean) {
+    if (!hide) return removeStyles();
+    addStyles();
+    checkForCurrenShortsActiveAndRemove();
 }
 
 onMessage('reload', _ => window.location.reload());
